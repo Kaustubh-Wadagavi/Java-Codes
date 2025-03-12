afc68acf57c494854e2086c2d467bd578d756a41
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2025-03-12 11:06:12 +0530

    1. Added logger. 2. Added creation date of supplies in CSV. 3. Modified the query to get the latest unused barcodes...

--- singh-health/getUnusedKitBarcodes.java
+++ singh-health/getUnusedKitBarcodes.java
@@ -1,13 +1,62 @@
-import java.io.*;
-import java.sql.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
-import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
+import java.util.Date;
+import java.util.Properties;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Multipart;
+import java.io.FileInputStream;
+
+class CustomLogger {
+    public static Logger createLogger(String className) {
+        Logger logger = Logger.getLogger(className);
+        logger.setUseParentHandlers(false);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter() {
+            private static final String FORMAT = "%1$tF %1$tT [%2$s] %3$s%n";
+            @Override
+            public synchronized String format(LogRecord record) {
+                return String.format(FORMAT,
+                        new java.util.Date(record.getMillis()),
+                        record.getLevel(),
+                        record.getMessage()
+                );
+            }
+        });
+        logger.addHandler(handler);
+        return logger;
+    }
+}
 
 class EmailSender {
+    private static final Logger logger = CustomLogger.createLogger(EmailSender.class.getName());
+    
     public static boolean sendEmailWithAttachment(String from, String password, String to, String smtpHost, String smtpPort, String tls, File attachment) {
+        logger.info("Preparing to send email...");
         Properties props = new Properties();
         props.put("mail.smtp.host", smtpHost);
         props.put("mail.smtp.port", smtpPort);
@@ -43,28 +92,34 @@ class EmailSender {
             multipart.addBodyPart(attachmentPart);
 
             message.setContent(multipart);
+            logger.info("Sending email...");
             Transport.send(message);
+            logger.info("Email sent successfully!");
             return true;
         } catch (MessagingException e) {
-            System.err.println("Email sending failed: " + e.getMessage());
+            logger.log(Level.SEVERE, "Email sending failed: {0}", e.getMessage());
             return false;
         }
     }
 }
 
 class getUnusedBarcodes {
+    private static final Logger logger = CustomLogger.createLogger(getUnusedBarcodes.class.getName());
+
     private static final String QUERY = """
-        SELECT 
-            COALESCE(site.name, 'Not Specified') AS "Supply Site Name",
-            cp.short_title AS "CP Short Title",
-            supply_types.name AS "Supply Type Name",
-            supply_items.barcode AS "Barcode"
-        FROM os_supplies supply
-        JOIN os_supply_types supply_types ON supply.type_id = supply_types.identifier
-        JOIN catissue_collection_protocol cp ON supply_types.cp_id = cp.identifier
-        JOIN os_supply_items supply_items ON supply.identifier = supply_items.supply_id
-        LEFT JOIN catissue_site site ON supply.site_id = site.identifier
-        WHERE supply_items.used_by IS NULL;
+    SELECT 
+  	    COALESCE(site.name, 'Not Specified') AS "Supply Site Name",
+  	    cp.short_title AS "CP Short Title",
+  	    supply_types.name AS "Supply Type Name",
+  	    supply_items.barcode AS "Barcode",
+  	    supply.creation_time AS "Created On"
+	FROM os_supplies supply
+  	    JOIN os_supply_types supply_types ON supply.type_id = supply_types.identifier
+  	    JOIN catissue_collection_protocol cp ON supply_types.cp_id = cp.identifier
+  	    JOIN os_supply_items supply_items ON supply.identifier = supply_items.supply_id
+  	    LEFT JOIN catissue_site site ON supply.site_id = site.identifier
+	WHERE supply_items.used_by IS NULL
+	    order by supply.creation_time desc;
     """;
 
     public static File generateUnusedKitBarcodesCSV(String user, String password, String host, String dbName) {
@@ -72,6 +127,7 @@ class getUnusedBarcodes {
         File csvFile = null;
 
         try {
+            logger.info("Connecting to database...");
             Class.forName("com.mysql.cj.jdbc.Driver");
             try (
                 Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
@@ -80,78 +136,61 @@ class getUnusedBarcodes {
             ) {
                 String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new java.util.Date());
                 csvFile = new File("unused_specimen_kit_barcodes_" + timestamp + ".csv");
-                
+                logger.info("Generating CSV file: " + csvFile.getAbsolutePath());
+
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFile))) {
                     writer.write("\"Supply Site Name\",\"CP Short Title\",\"Supply Type Name\",\"Barcode\"\n");
+                    int recordCount = 0;
                     while (resultSet.next()) {
                         writer.write("\"" + resultSet.getString("Supply Site Name") + "\"," +
                                    "\"" + resultSet.getString("CP Short Title") + "\"," +
                                    "\"" + resultSet.getString("Supply Type Name") + "\"," +
                                    "\"" + resultSet.getString("Barcode") + "\"\n");
+                        recordCount++;
                     }
+                    logger.info("CSV generation complete. Records written: " + recordCount);
                 }
             }
         } catch (ClassNotFoundException | SQLException | IOException e) {
-            System.err.println("Error: " + e.getMessage());
+            logger.log(Level.SEVERE, "Error: {0}", e.getMessage());
             return null;
         }
         return csvFile;
     }
 }
 
-class ConfigReader {
-    private final String configFilePath;
-    public ConfigReader(String configFilePath) {
-        this.configFilePath = configFilePath;
-    }
-    public Properties getConfigValues() {
-        Properties properties = new Properties();
-        try (FileInputStream input = new FileInputStream(configFilePath)) {
-            properties.load(input);
-        } catch (IOException e) {
-            System.err.println("Error reading config file: " + e.getMessage());
-            System.exit(1);
-        }
-        return properties;
-    }
-}
-
 public class getUnusedKitBarcodes {
+    private static final Logger logger = CustomLogger.createLogger(getUnusedKitBarcodes.class.getName());
+    
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.err.println("Error: Configuration file not provided. Usage: java getUnusedKitBarcodes <config-file>");
+            logger.severe("Error: Configuration file not provided. Usage: java getUnusedKitBarcodes <config-file>");
             System.exit(1);
         }
 
+        logger.info("Reading configuration file...");
         String configFile = args[0];
-        ConfigReader configReader = new ConfigReader(configFile);
-        Properties configValues = configReader.getConfigValues();
+        Properties configValues = new Properties();
+        try (FileInputStream input = new FileInputStream(configFile)) {
+            configValues.load(input);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error reading config file: {0}", e.getMessage());
+            System.exit(1);
+        }
 
-        String dbUser = configValues.getProperty("dbUser");
-        String dbPassword = configValues.getProperty("dbPassword");
-        String dbHost = configValues.getProperty("dbHost");
-        String dbName = configValues.getProperty("dbName");
-        String fromEmailId = configValues.getProperty("fromEmailId");
-        String emailPassword = configValues.getProperty("emailPassword");
-        String toEmailIds = configValues.getProperty("toEmailIds");
-        String smtpServerHost = configValues.getProperty("smtpServerHost");
-        String smtpServerPort = configValues.getProperty("smtpServerPort");
-        String startTLS = configValues.getProperty("startTLS");
-
-        File csvFile = getUnusedBarcodes.generateUnusedKitBarcodesCSV(dbUser, dbPassword, dbHost, dbName);
-
+        logger.info("Generating unused kit barcodes report...");
+        File csvFile = getUnusedBarcodes.generateUnusedKitBarcodesCSV(configValues.getProperty("dbUser"),
+                configValues.getProperty("dbPassword"),
+                configValues.getProperty("dbHost"),
+                configValues.getProperty("dbName"));
         if (csvFile != null) {
-            System.out.println("CSV file generated successfully: " + csvFile.getAbsolutePath());
-
-            boolean emailSent = EmailSender.sendEmailWithAttachment(
-                fromEmailId, emailPassword, toEmailIds, smtpServerHost, smtpServerPort, startTLS, csvFile);
-            if (emailSent) {
-                System.out.println("Email sent successfully!");
-            } else {
-                System.err.println("Failed to send email.");
-            }
-        } else {
-            System.err.println("Failed to generate CSV file.");
+            logger.info("Report generated successfully. Sending email...");
+            EmailSender.sendEmailWithAttachment(configValues.getProperty("fromEmailId"),
+                    configValues.getProperty("emailPassword"),
+                    configValues.getProperty("toEmailIds"),
+                    configValues.getProperty("smtpServerHost"),
+                    configValues.getProperty("smtpServerPort"),
+                    configValues.getProperty("startTLS"), csvFile);
         }
     }
 }
